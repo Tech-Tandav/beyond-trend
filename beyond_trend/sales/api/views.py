8e6c3dbac4a266528977596950c1a1318395d7a0
binaya.parajuli <binaya.parajuli@nicnepal.org>
2026-04-09 15:31:20 +0545

    fix

--- beyond_trend/sales/api/views.py
+++ beyond_trend/sales/api/views.py
@@ -8,7 +8,7 @@ from drf_spectacular.utils import (
 )
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from beyond_trend.core.excel import ExcelExportAPIView
@@ -90,7 +90,7 @@ class SaleViewSet(BaseModelViewSet):
             ),
         ],
     )
-    @action(detail=False, methods=["post"], url_path="checkout")
+    @action(detail=False, methods=["post"], url_path="checkout", permission_classes=[AllowAny])
     def checkout(self, request):
         """
         POS Checkout:
